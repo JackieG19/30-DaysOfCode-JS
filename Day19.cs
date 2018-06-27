f5e656f897d1ad3cd3034653ab94f29d3797eef6
Jackie <JGtheophila19@gmail.com>
2018-06-27 17:22:41 -0400

    Create Day19.cs

---
+++ Day19.cs
@@ -0,0 +1,13 @@
+using System;
+public interface AdvancedArithmetic{
+    int divisorSum(int n);
+}
+
+
+public class Calculator : AdvancedArithmetic
+{
+    public int divisorSum(int n)
+    {
+        return 0;
+    }
+}
