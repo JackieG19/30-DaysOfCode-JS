0cb6b6f3b6952b3109fb70006e62e48529255de5
Jackie <JGtheophila19@gmail.com>
2018-07-03 22:39:44 -0400

    Update Day02.js

--- Day02.js
+++ Day02.js
@@ -1,37 +1,13 @@
-// Operators
+/*
+Input Format:
+There are  lines of numeric input: 
+The first line has a double,  (the cost of the meal before tax and tip). 
+The second line has an integer,  (the percentage of  being added as tip). 
+The third line has an integer,  (the percentage of  being added as tax).
 
-'use strict';
-
-var _input = '';
-var _index = 0;
-process.stdin.on('data', (data) => { _input += data; });
-process.stdin.on('end', () => {
-	_input = _input.split(new RegExp('[\n ]+'));
-	main(+(_input[0]), +(_input[1]), +(_input[2]));    
-});
-
-function main(mealCost, tipPercent, taxPercent) {
-    process.stdin.resume();
-process.stdin.setEncoding('ascii');
-
-var input_stdin = "";
-var input_stdin_array = "";
-var input_currentline = 0;
-
-process.stdin.on('data', function (data) {
-    input_stdin += data;
-});
-
-process.stdin.on('end', function () {
-    input_stdin_array = input_stdin.split("\n");
-    main();    
-});
-
-function readLine() {
-    return input_stdin_array[input_currentline++];
-}
-
-/////////////// ignore above this line ////////////////////
+Output Format:
+Print The total meal cost is totalCost dollars., 
+where  is the rounded integer result of the entire bill ( with added tax and tip).*/
 
 
 function main() {
