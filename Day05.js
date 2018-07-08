10f6cbd951fb19c3d086557759aecbaf6dc6d587
Jackie <JGtheophila19@gmail.com>
2018-07-07 22:26:27 -0400

    Update Day05.js

--- Day05.js
+++ Day05.js
@@ -10,21 +10,6 @@ Input: A single integer, n.
 Constraints: 2 < n < 20
 Output: each line i contains the of n x i in the form: n x i = result.*/
 
-process.stdin.resume();
-process.stdin.setEncoding('ascii');
-
-var input_stdin = "";
-var input_stdin_array = "";
-var input_currentline = 0;
-
-process.stdin.on('data', function (data) {
-    input_stdin += data;
-});
-
-process.stdin.on('end', function () {
-    input_stdin_array = input_stdin.split("\n");
-    main();    
-});
 
 function readLine() {
     return input_stdin_array[input_currentline++];
